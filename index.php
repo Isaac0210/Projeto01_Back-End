d5d0cc560745938bbeaec40d136771e5b422c2a2
Isaac0210 <matiasisaac985@gmail.com>
2026-08-26 09:04:43 -0300

    Projeto1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto1</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+    <h1>Área de login</h1>
+    <input type="text" name = "login"> <br> <br>
+    <input type="text" name = "senha"> <br> <br>
+    <button>Entrar</button>
+    </form>
+</body>
+</html>
